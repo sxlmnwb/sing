a48ffa42a307e51a605e2e959f3669df7a5885c6
PuerNya <safarier@outlook.com>
2025-01-03 22:11:49 +0800

    Return success addr when multi-dial
    Co-authored-by: PuerNya <safarier@outlook.com>
    Signed-off-by: Salman Wahib <sxlmnwb@nodex.one>

--- common/network/multi.go
+++ common/network/multi.go
@@ -14,15 +14,20 @@ import (
 const DefaultFallbackDelay = 300 * time.Millisecond
 
 func DialSerial(ctx context.Context, dialer Dialer, network string, destination M.Socksaddr, destinationAddresses []netip.Addr) (net.Conn, error) {
+	conn, _, err := DialSerialWithAddr(ctx, dialer, network, destination, destinationAddresses)
+	return conn, err
+}
+
+func DialSerialWithAddr(ctx context.Context, dialer Dialer, network string, destination M.Socksaddr, destinationAddresses []netip.Addr) (net.Conn, netip.Addr, error) {
 	if parallelDialer, isParallel := dialer.(ParallelDialer); isParallel {
-		return parallelDialer.DialParallel(ctx, network, destination, destinationAddresses)
+		return parallelDialer.DialParallelWithAddr(ctx, network, destination, destinationAddresses)
 	}
 	length := len(destinationAddresses)
 	group := newTCPConnGroup(length)
 	for _, address := range destinationAddresses {
 		go func(address netip.Addr) {
 			conn, err := dialer.DialContext(ctx, network, M.SocksaddrFrom(address, destination.Port))
-			group.add(conn, err)
+			group.add(conn, address, err)
 		}(address)
 	}
 	return group.wait()
@@ -41,6 +46,11 @@ func ListenSerial(ctx context.Context, dialer Dialer, destination M.Socksaddr, d
 }
 
 func DialParallel(ctx context.Context, dialer Dialer, network string, destination M.Socksaddr, destinationAddresses []netip.Addr, preferIPv6 bool, fallbackDelay time.Duration) (net.Conn, error) {
+	conn, _, err := DialParallelWithAddr(ctx, dialer, network, destination, destinationAddresses, preferIPv6, fallbackDelay)
+	return conn, err
+}
+
+func DialParallelWithAddr(ctx context.Context, dialer Dialer, network string, destination M.Socksaddr, destinationAddresses []netip.Addr, preferIPv6 bool, fallbackDelay time.Duration) (net.Conn, netip.Addr, error) {
 	// kanged form net.Dial
 
 	if fallbackDelay == 0 {
@@ -57,7 +67,7 @@ func DialParallel(ctx context.Context, dialer Dialer, network string, destinatio
 		return address.Is6() && !address.Is4In6()
 	})
 	if len(addresses4) == 0 || len(addresses6) == 0 {
-		return DialSerial(ctx, dialer, network, destination, destinationAddresses)
+		return DialSerialWithAddr(ctx, dialer, network, destination, destinationAddresses)
 	}
 	var primaries, fallbacks []netip.Addr
 	if preferIPv6 {
@@ -69,6 +79,7 @@ func DialParallel(ctx context.Context, dialer Dialer, network string, destinatio
 	}
 	type dialResult struct {
 		net.Conn
+		netip.Addr
 		error
 		primary bool
 		done    bool
@@ -79,9 +90,9 @@ func DialParallel(ctx context.Context, dialer Dialer, network string, destinatio
 		if !primary {
 			ras = fallbacks
 		}
-		c, err := DialSerial(ctx, dialer, network, destination, ras)
+		c, addr, err := DialSerialWithAddr(ctx, dialer, network, destination, ras)
 		select {
-		case results <- dialResult{Conn: c, error: err, primary: primary, done: true}:
+		case results <- dialResult{Conn: c, Addr: addr, error: err, primary: primary, done: true}:
 		case <-returned:
 			if c != nil {
 				c.Close()
@@ -103,7 +114,7 @@ func DialParallel(ctx context.Context, dialer Dialer, network string, destinatio
 
 		case res := <-results:
 			if res.error == nil {
-				return res.Conn, nil
+				return res.Conn, res.Addr, nil
 			}
 			if res.primary {
 				primary = res
@@ -111,7 +122,7 @@ func DialParallel(ctx context.Context, dialer Dialer, network string, destinatio
 				fallback = res
 			}
 			if primary.done && fallback.done {
-				return nil, primary.error
+				return nil, netip.Addr{}, primary.error
 			}
 			if res.primary && fallbackTimer.Stop() {
 				fallbackTimer.Reset(0)
@@ -122,6 +133,7 @@ func DialParallel(ctx context.Context, dialer Dialer, network string, destinatio
 
 type tcpConn struct {
 	conn net.Conn
+	addr netip.Addr
 	err  error
 }
 
@@ -134,11 +146,11 @@ func newTCPConnGroup(count int) tcpConnGroup {
 	return tcpConnGroup{count, make(chan tcpConn, count)}
 }
 
-func (g *tcpConnGroup) add(conn net.Conn, err error) {
-	g.channel <- tcpConn{conn, err}
+func (g *tcpConnGroup) add(conn net.Conn, addr netip.Addr, err error) {
+	g.channel <- tcpConn{conn, addr, err}
 }
 
-func (g *tcpConnGroup) wait() (net.Conn, error) {
+func (g *tcpConnGroup) wait() (net.Conn, netip.Addr, error) {
 	var i int
 	var errors []error
 	defer func() {
@@ -147,11 +159,11 @@ func (g *tcpConnGroup) wait() (net.Conn, error) {
 	for i = 0; i < g.count; i++ {
 		tc := <-g.channel
 		if tc.err == nil {
-			return tc.conn, nil
+			return tc.conn, tc.addr, nil
 		}
 		errors = append(errors, tc.err)
 	}
-	return nil, E.Errors(errors...)
+	return nil, netip.Addr{}, E.Errors(errors...)
 }
 
 func (g *tcpConnGroup) close(index int) {
